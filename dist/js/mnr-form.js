4e3c0b1596d5891f99e567fbea2d9066ea320fef
milad nazari <mnrmilad@gmail.com>
2020-12-10 20:04:18 +0330

    fix data in select

--- dist/js/mnr-form.js
+++ dist/js/mnr-form.js
@@ -283,7 +283,7 @@ mnrfObj.multi.find("input:visible").on("keyup", function (event) {
 		}
 		if (new RegExp(sepreg).test($(this).val()) || event.keyCode === 13) {
 			splits = $(this).val().split(new RegExp(sepreg));
-			splits = splits.filter((item) => item);
+			splits = splits.filter(item => item);
 			obj.splits = obj.splits.concat(splits);
 			$(this).val("");
 			setMultiData(obj);
@@ -310,7 +310,14 @@ mnrfObj.multi.find("input:visible").on("keyup", function (event) {
 
 mnrfObj.multi.on("click", ".sub-tag", function () {
 	let obj = $(this).parents(".mnrf-multi").get(0);
-	obj.splits.splice(obj.splits.indexOf($(this).text()), 1);
+	let opt = $(this)
+	if($(this).attr('data')){
+		obj.splits = obj.splits.filter(function (i) {
+			return i.data !== opt.attr("data");
+		});
+	}else{
+		obj.splits.splice(obj.splits.indexOf(opt.text()), 1);
+	}
 	setMultiData(obj);
 });
 
@@ -318,19 +325,31 @@ function setMultiData(obj) {
 	obj.splits = obj.splits.filter(function (value, index, self) {
 		return self.indexOf(value) === index;
 	});
-	$(obj).find("input[type=hidden]").val(JSON.stringify(obj.splits));
 	$(obj).find(".sub-tags").empty();
 	if ($(obj).find(".sub-data").length) {
 		$(obj).find(".sub-option").removeClass("state-select");
 	}
+	let inputData = [];
 	$.each(obj.splits, function (i, item) {
+		let text
+		let data = false
+		if(typeof item == 'object'){
+			text = item.text
+			data = item.data
+			inputData.push(data);
+		}else{
+			text = item
+			inputData.push(text);
+		}
+		let tag = $('<span class="sub-tag"><span class="sub-close"><svg viewBox="0 0 311 311.07733" xmlns="http://www.w3.org/2000/svg"><path d="m16.035156 311.078125c-4.097656 0-8.195312-1.558594-11.308594-4.695313-6.25-6.25-6.25-16.382812 0-22.632812l279.0625-279.0625c6.25-6.25 16.382813-6.25 22.632813 0s6.25 16.382812 0 22.636719l-279.058594 279.058593c-3.136719 3.117188-7.234375 4.695313-11.328125 4.695313zm0 0"/><path d="m295.117188 311.078125c-4.097657 0-8.191407-1.558594-11.308594-4.695313l-279.082032-279.058593c-6.25-6.253907-6.25-16.386719 0-22.636719s16.382813-6.25 22.636719 0l279.058594 279.0625c6.25 6.25 6.25 16.382812 0 22.632812-3.136719 3.117188-7.230469 4.695313-11.304687 4.695313zm0 0"/></svg></span>' +
+		text +
+		"</span>")
+		if(data !== false){
+			tag.attr('data',data)
+		}
 		$(obj)
 			.find(".sub-tags")
-			.append(
-				'<span class="sub-tag"><span class="sub-close"><svg viewBox="0 0 311 311.07733" xmlns="http://www.w3.org/2000/svg"><path d="m16.035156 311.078125c-4.097656 0-8.195312-1.558594-11.308594-4.695313-6.25-6.25-6.25-16.382812 0-22.632812l279.0625-279.0625c6.25-6.25 16.382813-6.25 22.632813 0s6.25 16.382812 0 22.636719l-279.058594 279.058593c-3.136719 3.117188-7.234375 4.695313-11.328125 4.695313zm0 0"/><path d="m295.117188 311.078125c-4.097657 0-8.191407-1.558594-11.308594-4.695313l-279.082032-279.058593c-6.25-6.253907-6.25-16.386719 0-22.636719s16.382813-6.25 22.636719 0l279.058594 279.0625c6.25 6.25 6.25 16.382812 0 22.632812-3.136719 3.117188-7.230469 4.695313-11.304687 4.695313zm0 0"/></svg></span>' +
-					item +
-					"</span>"
-			);
+			.append(tag);
 		if ($(obj).find(".sub-data").length) {
 			// obj.find('.sub-option').attr('data',item).addClass('state-select')
 			// obj.find('.sub-option').attr('data',item).addClass('state-select')
@@ -338,12 +357,13 @@ function setMultiData(obj) {
 				.find(".sub-option")
 				.filter(function () {
 					return (
-						$(this).text() == item || $(this).attr("data") == item
+						$(this).text() == text || (data !== false && $(this).attr("data") == data)
 					);
 				});
 			selectedOpt.addClass("state-select");
 		}
 	});
+	$(obj).find("input[type=hidden]").val(JSON.stringify(inputData));
 }
 
 mnrfObj.multi.find(".sub-toggleData").on("click", function () {
@@ -369,9 +389,15 @@ mnrfObj.multi.find(".sub-option").on("click", function () {
 	// obj.splits = typeof mnrfObj == "undefined" ? [] : obj.splits;
 	if (opt.attr("data")) {
 		if (opt.is(".state-select")) {
-			obj.splits.splice(obj.splits.indexOf(opt.attr("data")), 1);
+			// obj.splits.splice(obj.splits.indexOf(opt.attr("data")), 1);
+			obj.splits = obj.splits.filter(function (i) {
+				return i.data !== opt.attr("data");
+			});
 		} else {
-			obj.splits.push(opt.attr("data"));
+			obj.splits.push({
+				data:opt.attr("data"),
+				text:opt.text()
+			});
 		}
 	} else {
 		if (opt.is(".state-select")) {

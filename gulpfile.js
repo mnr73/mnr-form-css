00c9ce326476d1262e98f0131b88803b2cc072e4
milad nazari <mnrmilad@gmail.com>
2020-12-14 16:28:28 +0330

    improve checkbox and radio

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 const { src, dest, parallel, series, watch } = require("gulp");
 const sass = require("gulp-dart-sass");
 const autoprefixer = require("autoprefixer");
-const clean = require("postcss-clean");
 const sourcemaps = require("gulp-sourcemaps");
 const postcss = require("gulp-postcss");
 const minify = require("gulp-minify");
@@ -32,59 +31,60 @@ function css() {
 				}),
 			])
 		)
-		.pipe(sourcemaps.write("."))
 		.pipe(
 			cleanCSS({
 				level: 2,
 				format: "beautify",
 			})
 		)
-		.pipe(dest("./dist/css"))
 		.pipe(
-			cleanCSS({
-				level: 2,
+			sourcemaps.mapSources(function (sourcePath, file) {
+				return "../../src/scss/mnr-form.scss";
 			})
 		)
+		.pipe(sourcemaps.write("."))
+		.pipe(dest("./dist/css"));
+}
+
+function mincss() {
+	return src("./dist/css/mnr-form.css")
+		.pipe(sourcemaps.init())
+		.pipe(cleanCSS({ level: 0 }))
 		.pipe(
 			rename({
 				suffix: ".min",
 			})
 		)
-		.pipe(dest("./dist/css/"));
+		.pipe(
+			sourcemaps.mapSources(function (sourcePath, file) {
+				return "../../src/scss/mnr-form.scss";
+			})
+		)
+		.pipe(sourcemaps.write("."))
+		.pipe(dest("./dist/css"));
 }
-
-// function css() {
-// 	return src('./src/css/*.css')
-// 		.pipe(sourcemaps.init())
-// 		.pipe(postcss([autoprefixer({
-// 			grid: 'autoplace',
-// 			flexbox: true,
-// 		})]))
-// 		.pipe(cleanCSS({level: 2}))
-// 		.pipe(sourcemaps.write('.'))
-// 		.pipe(dest('./dist/css'))
-// }
 
 function js() {
 	return src([
-	"./src/js/basic.js",
-	"./src/js/checkAndRadio.js",
-	"./src/js/input.js",
-	"./src/js/multiple.js",
-	"./src/js/textarea.js",
-	"./src/js/functions.js",
+		"./src/js/basic.js",
+		"./src/js/checkAndRadio.js",
+		"./src/js/input.js",
+		"./src/js/multiple.js",
+		"./src/js/textarea.js",
+		"./src/js/functions.js",
 	])
-	.pipe(concat("mnr-form.js"))
-	.pipe(minify())
-	.pipe(dest("./dist/js"));
+		.pipe(concat("mnr-form.js"))
+		.pipe(minify())
+		.pipe(dest("./dist/js"));
 }
 
 exports.js = js;
 exports.scss = scss;
 exports.css = css;
-exports.default = parallel(series(scss, css), js);
+exports.mincss = mincss;
+exports.default = parallel(series(scss, css, mincss), js);
 
 // console.log('done');
 
-watch("./src/scss/**/*.scss", series(scss, css));
+watch("./src/scss/**/*.scss", series(scss, css, mincss));
 watch("./src/js/**/*.js", series(js));
